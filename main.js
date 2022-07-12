fa0032ee6c0762ebef9ca13ad3154da52d582f3d
Lenchata <101400465+Lenchata@users.noreply.github.com>
2022-07-12 17:05:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", message => {
+    if (message.content === "!ping") {
+        message.reply(`Pong! Latency is ${client.ws.ping}ms.`);
+    }
+}
+);
+
+client.login("OTY3OTUxNDczMDg4NTYxMjEy.GHAaFt.DthR_9cIW3eZwxqm-35LjPUSXbi8Ytqj0PNQi8");
